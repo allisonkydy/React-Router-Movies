994e9675328ddd7306f82ff6a0452ed8d51e56cb
allisonkydy <allisnelly@gmail.com>
2019-08-20 15:46:43 -0500

    add images and change styles

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,25 +1,31 @@
 import React from 'react';
 
 const MovieCard = props => {
-  const { title, director, metascore, stars } = props.movie;
+  const { title, director, metascore, stars, id } = props.movie;
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+      <img src={require(`../assets/${id}.jpg`)} alt={`${title}`}/>
+      <div>
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      
     </div>
   )
 };
 
 export default MovieCard;
+
+// ../assets/${id}.jpg

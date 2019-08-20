c063b9fda17f7ed1f49d239421800aba103d37d1
allisonkydy <allisnelly@gmail.com>
2019-08-20 14:40:05 -0500

    turn saved movies into links

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -19,11 +19,10 @@ const Movie = props => {
 
   },[id]);
   
-  // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -32,7 +31,7 @@ const Movie = props => {
   return (
     <div className="save-wrapper">
       <MovieCard movie={movie}/>
-      <button onClick={() => props.addToSavedList(movie)} className="save-button">Save</button>
+      <button onClick={saveMovie} className="save-button">Save</button>
     </div>
   );
 }
